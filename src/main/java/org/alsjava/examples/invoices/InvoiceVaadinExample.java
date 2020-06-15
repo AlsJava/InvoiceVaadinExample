b02fd5db1e45ae1f28b9f13b3d8581d0dba1685c
Aluis Marte <aluismarte@gmail.com>
2020-06-14 20:21:01 -0400

    Adicionando elementos basicos de inicio del proyecto.

--- src/main/java/org/alsjava/examples/invoices/InvoiceVaadinExample.java
+++ src/main/java/org/alsjava/examples/invoices/InvoiceVaadinExample.java
@@ -1,7 +1,15 @@
 package org.alsjava.examples.invoices;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by aluis on 6/14/20.
  */
+@SpringBootApplication
 public class InvoiceVaadinExample {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceVaadinExample.class, args);
+    }
 }
